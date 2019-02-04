c574eccbea8795a41f25ccf0358f3f09e7b62caf
sridatha <sridathatumati@gmail.com>
2019-02-04 05:05:24 -0800

    classroom

---
+++ classtask.py
@@ -0,0 +1,13 @@
+"""from array import array
+from netaddr import *
+
+def subnet_calculator():
+    class_A = ('10.0.0.1')
+    class_B = ('172.16.0.1')
+    class_C = ('192.168.0.1')
+
+
+    print "CLASS A = 10.0.0.1"
+    print "CLASS B = 172.16.0.1"
+    print "CLASS C = 192.168.0.1"
+    print "---------------------"
